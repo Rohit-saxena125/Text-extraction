15b48a4688838682b08d0f219c4103b5ed7f14c4
dariklinux <darik@darik.darik>
2022-06-29 12:42:45 +0530

    update

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
-'use strict'
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const pdfparse = require('pdf-parse');
+"use strict";
+const express = require("express");
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+const pdfparse = require("pdf-parse");
 const app = express();
-app.use("/",express.static("public"));
+app.use("/", express.static("public"));
 app.use(fileUpload());
-// app.post('/upload', (req, res) => {
-//     if (req.files === null) {
-//       return res.status(400).json({ msg: 'No file uploaded' });
-//     }
-  
-//     const file = req.files.file;
-  
-//     file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-//       if (err) {
-//         console.error(err);
-//         return res.status(500).send(err);
-//       }
-  
-//       res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-//     });
-//   });
-  
-app.post("/extract-text",(req,res)=>
-{
-    if(!req.files && !req.files.pdfFile)
-    {
-        res.send("No file uploaded");
-    }
-    pdfparse(req.files.pdfFile).then(result=>
+app.post("/extract-text", (req, res) => {
+  if (req.files.pdfFile === null) {
+    return res.status(400).send("No file uploaded");
+  }
+  const file = req.files.pdfFile;
+  pdfparse(file).then(
+    (result) => {
+      res.send(result.text);
+      // result.text send raw text
+      fs.writeFileSync("public/text.txt", result.text);
+      fs.readFile("public/text.txt", "utf8", (err, data) => {
+        if (err) throw err;
+        var lines = data.split("\n");
+        let newText = "";
+        for (var i = 0; i < lines.length; i++) 
         {
-            res.send(result.text);
-            const jsondata = JSON.stringify(result.text);
-            // console.log(jsondata);
-            // extract date from pdf and display in console and send to frontend as json object to display in frontend as date picker except all format of date is dd-mm-yyyy so we need to convert it to this format
-            const date = result.text.match(/\d{2}-\d{2}-\d{4}/g);
-
-            const date1 = result.text.match(/\d{2}\/\d{2}\/\d{4}/g);
-
-            console.log(date);
-            // extract after date text from pdf and display in console and send to frontend as json object to display in frontend 
-            const afterdate = result.text.match(/\d{2}\/\d{2}\/\d{4}\s[a-zA-Z]{10}.*/g);
-            console.log(afterdate);
-
-        });
-}); 
-app.listen(3000);
+          if ((lines[i].match(/\d{2}-\d{2}-\d{4}/g) ||lines[i].match(/\d{2}\/\d{2}\/\d{4}/g))||(lines[i].match(/\d{2}-\d{1}-\d{4}/g)||lines[i].match(/\d{2}\/\d{1}\/\d{4}/g)))
+          {
+            newText += lines[i] + "\n";  
+          } 
+        }
+        fs.appendFileSync("public/text.txt", newText);
+            console.log(newText); 
+      }); //.then()     //delete file
+    }
+  );
+});
+app.listen(4000);
+// result.text.match(/\d{2}\/\d{2}\/\d{4}.*/g)
