3640ed4e99ee3394cd58df1e8055ec40eff1c06c
dariklinux <darik@darik.darik>
2022-07-07 09:30:39 +0530

    updated task

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 "use strict";
 const express = require("express");
+// const validator = require('validator');
 const fs = require("fs");
 const fileUpload = require("express-fileupload");
 const pdfparse = require("pdf-parse");
+// const { isDate } = require("util/types");
 const app = express();
 app.use("/", express.static("public"));
 app.use(fileUpload());
@@ -22,16 +24,37 @@ app.post("/extract-text", (req, res) => {
         let newText = "";
         for (var i = 0; i < lines.length; i++) 
         {
-          if ((lines[i].match(/\d{2}-\d{2}-\d{4}/g) ||lines[i].match(/\d{2}\/\d{2}\/\d{4}/g))||(lines[i].match(/\d{2}-\d{1}-\d{4}/g)||lines[i].match(/\d{2}\/\d{1}\/\d{4}/g)))
+          if (lines[i].match(/\d{1,2}-\d{1,2}-\d{4}/g) ||lines[i].match(/\d{1,2}\/\d{1,2}\/\d{4}/g))
           {
             newText += lines[i] + "\n";  
           } 
         }
         fs.appendFileSync("public/text.txt", newText);
             console.log(newText); 
-      }); //.then()     //delete file
-    }
-  );
+      })//delete file
+      // if (validator.isDate(newText))
+      // {
+      //   console.log("date");
+      // }
+      // else if (validator.isCurrency(newText))
+      // {
+      //   console.log("currency");
+      // }
+      // else if(validator.isDate(newText)&&validator.isCurrency(newText))
+      // {
+      //   console.log("date and currency");
+      // }
+      // else
+      // {
+      //   console.log("not date or currency");
+      // }
+     });//.then(
+    //     fs.unlink("public/text.txt", (err) => {
+    //         if (err) throw err;
+    //         console.log("File deleted!");
+    //       }
+    //     )
+    //   ); 
 });
 app.listen(4000);
-// result.text.match(/\d{2}\/\d{2}\/\d{4}.*/g)
+
